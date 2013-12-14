fdddd31b7aeab78f8106692e72290177c6c4dd5f
kingdon <kinpauln@126.com>
2013-12-14 10:57:50 +0800

    2nd commit

--- RoadLamps.Service/RoadLampsService.cs
+++ RoadLamps.Service/RoadLampsService.cs
@@ -1,6 +1,7 @@
 ﻿using RoadLamps.Contract;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
@@ -13,25 +14,28 @@ namespace RoadLamps.Service
 {
     public class RoadLampsService : IRoadLampsService
     {
+        public static ListView DisplayPanel { get; set; }
+        public static SynchronizationContext synContext { get; set; }
+
+        #region ICalculator 成员
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
         }
 
-        #region ICalculator 成员
-
-        public static ListView DisplayPanel { get; set; }
-        
-        public string GetData(int value)
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
         {
-            return string.Format("You entered: {0}", value);
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
         }
-
-        #region ICalculator 成员
-
-        public static ListView DisplayPanel { get; set; }
-
-        public static SynchronizationContext synContext{ get; set; }
 
         public double Add(double x, double y)
         {
@@ -51,9 +55,8 @@ namespace RoadLamps.Service
             return x + y;
         }
 
-        #endregion
-
-        public bool CreateFile() {
+        public bool CreateFile()
+        {
             string file = @"d:\efg.txt";
             string content = "hahhaha";
             if (File.Exists(file))
@@ -71,72 +74,6 @@ namespace RoadLamps.Service
             }
             return true;
         }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
-        public static SynchronizationContext synContext { get; set; }
-
-        public double Add(double x, double y)
-        {
-            DateTime calculationtime = System.DateTime.Now;
-            string threadID = Thread.CurrentThread.ManagedThreadId.ToString();
-            double result = x + y;
-            object remoteObject;
-            OperationContext.Current.IncomingMessageProperties.TryGetValue(RemoteEndpointMessageProperty.Name, out remoteObject);
-            string ipAddress = (remoteObject as RemoteEndpointMessageProperty).Address;
-            synContext.Post(
-            delegate
-            {
-                ListViewItem li = new ListViewItem(new string[] { string.Format("the result is{2} when x={0} and y={1}", x, y, result), ipAddress, calculationtime.ToString("hh:MM:ss"), threadID });
-                DisplayPanel.Items.Add(li);
-            }, null);
-            Thread.Sleep(5000);
-            return x + y;
-        }
-
         #endregion
-
-        //public bool CreateFile()
-        //{
-        //    string file = @"d:\efg.txt";
-        //    string content = "hahhaha";
-        //    if (File.Exists(file))
-        //    {
-        //        MessageBox.Show("存在此文件!");
-        //    }
-        //    else
-        //    {
-        //        FileStream myFs = new FileStream(file, FileMode.Create);
-        //        StreamWriter mySw = new StreamWriter(myFs);
-        //        mySw.Write(content);
-        //        mySw.Close();
-        //        myFs.Close();
-        //        MessageBox.Show("写入成功");
-        //    }
-        //    return true;
-        //}
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
     }
 }
